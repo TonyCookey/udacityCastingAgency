7840735db889f1f6f96f15c571f645661f84b909
Tony Cookey <tonyiroegbu59@gmail.com>
2020-08-06 22:35:55 +0100

    Fix: Updated the app.py to api.py and renamed the APP to app to allwo gunicorn run properly

--- api.py
+++ api.py
@@ -268,8 +268,8 @@ def create_app(test_config=None):
 
   return app
 
-APP = create_app()
+app = create_app()
 
 
 if __name__ == '__main__':
-    APP.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
